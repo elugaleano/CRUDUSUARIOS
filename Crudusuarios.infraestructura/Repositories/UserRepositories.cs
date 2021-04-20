891f75cd1616c0552350ef240b231c4e63eab5d4
Eluhuei Galeano <eluhuei95@hotmail.com>
2021-04-19 21:38:29 -0300

    user repositories, dynamics

---
+++ Crudusuarios.infraestructura/Repositories/UserRepositories.cs
@@ -0,0 +1,27 @@
+﻿using Crudusuarios.core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crudusuarios.infraestructura.Repositories
+{
+    public class UserRepositories
+    {
+        //ESTO ES DINAMYCS
+        public IEnumerable<User> getUsers() 
+        {
+            var usuarios = Enumerable.Range(1, 10).Select(x => new User
+            {
+                nombre = "Eluhuei",
+                apellido = "Galeano",
+                edad = 25
+
+                //LA FK QUEDA AFUERA 
+            });
+          return usuarios;
+            
+        }
+        
+    }
+}
