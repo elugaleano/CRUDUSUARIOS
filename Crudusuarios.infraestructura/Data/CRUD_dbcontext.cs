73e3f47f53796c4ccb0e3f5b8eb6cb8e5c0ca1cc
Eluhuei Galeano <eluhuei95@hotmail.com>
2021-04-21 21:12:19 -0300

    dbcontext

---
+++ Crudusuarios.infraestructura/Data/CRUD_dbcontext.cs
@@ -0,0 +1,23 @@
+﻿using Crudusuarios.core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crudusuarios.infraestructura.Data
+{
+    public class CRUD_dbcontext : DbContext
+    {
+        public virtual DbSet<Jerarquia> jerarquia{ get; set ;}
+        public virtual DbSet<User> User { get; set; }
+
+        public CRUD_dbcontext()
+        {
+
+        }
+
+        public CRUD_dbcontext(DbContextOptions<CRUD_dbcontext> options)
+        : base(options) { }
+
+    }
+} 
